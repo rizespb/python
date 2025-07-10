2beb9099e30ef223cabbd1793ce9197face41952
rizespb <rizespb@yandex.ru>
2025-07-10 10:16:25 +0300

    10.07.2025 10:17

--- Python_code/tests_for_script.py
+++ Python_code/tests_for_script.py
@@ -1,17 +1,17 @@
 import unittest
 
-import my_script
+import script
 
 
 class TestCap(unittest.TestCase):
     def test_one_word(self):
         text = "python"
-        result = my_script.cap_text(text)
+        result = script.cap_text(text)
         self.assertEqual(result, "Python")
 
     def test_multiple_words(self):
         text = "monty python"
-        result = my_script.cap_text(text)
+        result = script.cap_text(text)
         self.assertEqual(result, "Monty Python")
 
 
